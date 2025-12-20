0bd59696da82748eedb991b489059ec43bff86cd
anuj266 <anujudaywal@gmail.com>
2025-12-20 12:50:28 +0530

    [Anuj Udaywal] Format.Foramated scnerio based problem

--- core-java-practice/scenario-based/CoffeeShop.java
+++ core-java-practice/scenario-based/CoffeeShop.java
@@ -1,4 +1,9 @@
-package scenario_based;
+// The line `package scenario_based;` is declaring the package name for the Java class `CoffeeShop`. In
+// Java, packages are used to organize classes into namespaces, making it easier to manage and maintain
+// code. By specifying the package name at the beginning of the file, the Java class `CoffeeShop`
+// belongs to the `scenario_based` package. This helps in avoiding naming conflicts and provides a
+// structured way to organize related classes.
+// package scenario_based;
 import java.util.Scanner;
 class CoffeeShop{
 	public static void main(String[] args) {
@@ -31,6 +36,12 @@ class CoffeeShop{
 			double totalPrice = price*quantity;
 			double totalPriceIncludingGST = totalPrice + totalPrice*gst;
 			System.out.printf("Total Price (Including GST) : $%.2f%n", totalPriceIncludingGST);
+			System.out.println("Bill Generated Successfully");
+			System.out.println("Coffee Type : " + typeOfCoffee);
+			System.out.println("Quantity : " + quantity);
+			System.out.println("Total Price : $" + totalPrice);
+			System.out.printf("Total Price (Including GST) : $%.2f%n", totalPriceIncludingGST);
+
 			sc.close();
 			
 			
